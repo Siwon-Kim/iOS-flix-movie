b3e09e6013206ec270858a09064fef0d7b268e1c
Siwon  Kim <c1cool11@naver.com>
2022-02-08 01:04:02 -0500

    Added MovieDetailsViewController

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -17,6 +17,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.rowHeight = 135
+        
         tableView.dataSource = self
         tableView.delegate = self
         
@@ -68,4 +70,17 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
+    
+    override func prepare(for segue:UIStoryboardSegue, sender: Any?) {
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+            }
 }
