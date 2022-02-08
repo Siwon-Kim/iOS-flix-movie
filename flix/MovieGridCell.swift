631e8ec5743eb782b2e4d3aea75ba9ffa35dd397
Siwon  Kim <c1cool11@naver.com>
2022-02-08 17:25:00 -0500

    Added navigation controller
    Navigation controller for Superhero section

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by 김시원 on 2/8/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
